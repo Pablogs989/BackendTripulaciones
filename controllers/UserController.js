1ae3602d8aa2d942d31ba1d423aaa2529fca8800
Pablogs989 <pablogonsan2001@gmail.com>
2024-06-20 11:46:32 +0200

    login

---
+++ controllers/UserController.js
@@ -0,0 +1,104 @@
+const User = require("../models/User.js");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const { JWT_SECRET } = process.env;
+
+const UserController = {
+    async register(req, res, next) {
+        try {
+            const newUser = await User.findOne({
+                email: req.body.email,
+            });
+            if (!newUser) {
+                if (!req.body.password) {
+                    return res.status(400).send("Complete the password field");
+                }
+                const password = await bcrypt.hash(req.body.password, 10);
+                if (req.file) req.body.profilePic = req.file.filename;
+                await User.create({
+                    ...req.body,
+                    password,
+                    role: "user",
+                });
+            } else {
+                return res.status(400).send("User already exists");
+            }
+            res.status(201).send({
+                message:
+                    "Welcome, you are one step away from registering, check your email to confirm your registration",
+            });
+        } catch (error) {
+            next(error);
+        }
+    },
+    async login(req, res, next) {
+        try {
+            const user = await User.findOne({
+                email: req.body.email,
+            })
+                .populate({
+                    path: "postsId",
+                    populate: {
+                        path: "userId",
+                    },
+                })
+                .populate("commentsId", "text")
+                .populate("followers")
+                .populate("following");
+            if (!user) {
+                return res.status(400).send("Invalid email or password");
+            }
+            if (
+                !req.body.password ||
+                !bcrypt.compareSync(req.body.password, user.password)
+            ) {
+                return res.status(400).send("Invalid email or password");
+            }
+            if (!user.confirmed) {
+                return res
+                    .status(400)
+                    .send({ message: "You should confirm your email" });
+            }
+            const token = jwt.sign({ _id: user._id }, JWT_SECRET);
+            if (user.tokens.length > 4) user.tokens.shift();
+            user.tokens.push(token);
+            await user.save();
+            res.status(200).send({
+                message: "Welcome " + user.name,
+                token,
+                user,
+            });
+        } catch (error) {
+            next(error);
+        }
+    },
+    async logout(req, res) {
+        try {
+            await User.findByIdAndUpdate(req.user._id, {
+                $pull: { tokens: req.headers.authorization },
+            });
+            res.send({ message: "Logged out" });
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({
+                message: "There was a problem logging out the user",
+            });
+        }
+    },
+
+    async confirm(req, res) {
+        try {
+            const token = req.params.emailToken;
+            const payload = jwt.verify(token, JWT_SECRET);
+            const email = await User.findOne({ email: payload.email });
+            await User.findByIdAndUpdate(email._id, { confirmed: true });
+            res.status(201).send({ message: "User confirmed" });
+        } catch (error) {
+            console.error(error);
+        }
+    },
+
+};
+
+module.exports = UserController;
---
+++ middlewere/authentication.js
@@ -0,0 +1,31 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const { JWT_SECRET } = process.env
+
+const authentication = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization;
+        const payload = jwt.verify(token, JWT_SECRET);
+        const user = await User.findOne({ _id: payload._id, tokens: token });
+        if (!user) {
+            return res.status(401).send({ message: 'You are not authorized' });
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send({ error, message: 'There was a problem with the token' })
+    }
+}
+const isAdmin = async (req, res, next) => {
+    const admins = ['admin', 'superadmin'];
+    if (!admins.includes(req.user.role)) {
+        return res.status(403).send({
+            message: "You don't have permits"
+        });
+    }
+    next();
+}
+
+module.exports = { authentication, isAdmin, isAuthorComment, isAuthorPost };
