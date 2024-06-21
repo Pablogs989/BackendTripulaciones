0834ed51b56e261d5e3ea00baf15304cf385c17b
Pablogs989 <pablogonsan2001@gmail.com>
2024-06-21 12:42:15 +0200

    Update Suplier.js

--- models/Suplier.js
+++ models/Suplier.js
@@ -5,7 +5,7 @@ const SuplierSchema = new mongoose.Schema({
         type: String,
         required: [true, "Please fill in the CIF field"],
     },
-    name: {
+    company_name: {
         type: String,
         required: [true, "Please fill in the name field"],
     },
@@ -13,13 +13,56 @@ const SuplierSchema = new mongoose.Schema({
         type: String,
         required: [true, "Please fill in the password field"],
     },
+    address_contact: {
+        type: String,
+        required: [true, "Please fill in the address field"],
+    },
+    country: {
+        type: String,
+        required: [true, "Please fill in the country field"],
+    },
+    email: {
+        type: String,
+        match: [/.+\@.+\..+/, "Invalid email"],
+        required: [true, "Please fill in the email field"],
+    },
+    phone_prefx: {
+        type: Number,
+        required: [true, "Please fill in the phone prefix field"],
+    },
+    phone_number: {
+        type: Number,
+        required: [true, "Please fill in the phone number field"],
+    },
+    type_collab: {
+        type: String,
+        required: [true, "Please fill in the type of collaboration field"],
+        enum: ["Platinum", "Gold", "Silver"]
+    },
+    interests: {
+        type: [{
+            type: String,
+            enum: [
+                "Tecnología",
+                "Gestión de Proyectos",
+                "Agile",
+                "Softskills",
+                "Marketing Digital",
+                "Negocios",
+                "Emprendimiento",
+                "Educación",
+                "Formación",
+                "Salud y Bienestar",
+                "Creatividad",
+                "Diseño"
+            ]
+        }],
+    },
     employes: {
         type: Number,
         required: [true, "Please fill in the employes field"],
     },
-    tokens: [],
-    confirmed: { type: Boolean }
-    
+    tokens: [],    
 }, { timestamps: true });
 
 const Suplier = mongoose.model('Suplier', SuplierSchema);
