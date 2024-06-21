f35877d68a2b55ee96cebf21a4df6c5f78a8c72e
Pablogs989 <pablogonsan2001@gmail.com>
2024-06-21 11:26:33 +0200

    new user

--- models/User.js
+++ models/User.js
@@ -5,6 +5,10 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: [true, "Please fill in the name field"],
     },
+    surename: {
+        type: String,
+        required: [true, "Please fill in the name field"],
+    },
     email: {
         type: String,
         match: [/.+\@.+\..+/, "Invalid email"],
@@ -15,8 +19,91 @@ const UserSchema = new mongoose.Schema({
         required: [true, "Please fill in the password field"],
     },
     tokens: [],
-    confirmed: { type: Boolean }
-    
+    confirmed: { type: Boolean },
+    phone_prefx: {
+        type: Number,
+        required: [true, "Please fill in the phone prefix field"],
+    },
+    phone_number: {
+        type: Number,
+        required: [true, "Please fill in the phone number field"],
+    },
+    address: {
+        type: String,
+        required: [true, "Please fill in the address field"],
+    },
+    zip_code: {
+        type: Number,
+        required: [true, "Please fill in the zip code field"],
+    },
+    city: {
+        type: String,
+        required: [true, "Please fill in the city field"],
+    },
+    country: {
+        type: String,
+        required: [true, "Please fill in the country field"],
+    },
+    user_type: {
+        type: String,
+        required: [true, "Please fill in the user type field"],
+    },
+    url_linkedin: {
+        type: String,
+        required: [true, "Please fill in the linkedin field"],
+    },
+    allergies: {
+        type: [{
+            type: String,
+            enum: [
+                "Gluten",
+                "Lácteos",
+                "Huevos",
+                "Frutos secos",
+                "Cacahuetes",
+                "Mariscos",
+                "Pescado",
+                "Soja",
+                "Sésamo",
+                "Mostaza",
+                "Apio",
+                "Sulfitos",
+                "Altramuz",
+                "Moluscos",
+                "Azucar",
+                "Sal"
+            ]
+        }],
+    },
+    interests: {
+        type: [{
+            type: String,
+            enum: [
+                "Tecnología",
+                "Gestión de Proyectos",
+                "Agile",
+                "Softskills",
+                "Marketing Digital",
+                "Negocios",
+                "Emprendimiento",
+                "Educación",
+                "Formación",
+                "Salud y Bienestar",
+                "Creatividad",
+                "Diseño"
+            ]
+        }],
+    },
+    food_preferences: {
+        type: [{
+            type: String,
+            enum: [
+                "vegetariano",
+                "vegano",
+                "omnivore"
+            ]
+        }],
+    },
 }, { timestamps: true });
 
 const User = mongoose.model('User', UserSchema);
