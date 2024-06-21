93c64d5e9a5551b432955523c0075aec4226c734
manudana11 <manueldanapallardo@gmail.com>
2024-06-21 14:15:25 +0200

    getuserbyid

--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,6 @@ const { authentication } = require('../middleware/authentication.js')
 router.post('/', UserController.register)
 router.post('/login', UserController.login)
 router.delete('/logout', authentication, UserController.logout)
+router.get('/id/:_id', UserController.getUserById)
 
 module.exports = router
