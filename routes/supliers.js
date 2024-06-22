87ed99ccf77f04b2bcc601909117fbb1481431e3
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2024-06-22 13:13:08 +0200

    se crea controlador para obtener company por nombre y se agrega la rutia correspondiente

--- routes/supliers.js
+++ routes/supliers.js
@@ -1,10 +1,12 @@
-const express = require('express')
-const SuplierController = require('../controllers/SuplierController.js')
-const router = express.Router()
-const { authentication } = require('../middleware/authentication.js')
+const express = require("express");
+const SuplierController = require("../controllers/SuplierController.js");
+const router = express.Router();
+const { authentication } = require("../middleware/authentication.js");
 
-router.post('/', SuplierController.register)
-router.post('/login', SuplierController.login)
-router.delete('/logout', authentication, SuplierController.logout)
+router.post("/", SuplierController.register);
+router.post("/login", SuplierController.login);
+router.get("/companies", SuplierController.getAllCompanies);//revisar por que no me deja entrar si yo estoy logueado
+router.get("/company/:companyName", SuplierController.getCompanyByName)
+router.delete("/logout", authentication, SuplierController.logout);// no me deja desloguearme por el token
 
-module.exports = router
+module.exports = router;
