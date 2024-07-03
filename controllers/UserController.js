c5ca33410c4901e1a4f0bc811286d8c61a133a80
Pablogs989 <pablogonsan2001@gmail.com>
2024-07-03 15:12:33 +0200

    user update

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -286,9 +286,8 @@ const UserController = {
                 const mainDirPath = path.join(__dirname, '..');
                 req.body.avatar_url = await uploadImageToImgur(mainDirPath +"/"+imagePath) || req.file.filename
             }
-            const password = await bcrypt.hash(req.body.password, 10);
             const completed = req.user.completed = true;
-            const user = await User.findByIdAndUpdate(req.user._id, {...req.body, password, completed}, { new: true });
+            const user = await User.findByIdAndUpdate(req.user._id, {...req.body, completed}, { new: true });
             res.send(user);
         }
         catch (error) {
